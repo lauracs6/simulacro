14d16357599cb7416294a877faa2ba5f885d873e
Laura Cabrera Sagrera <lauracabrerasagrera@gmail.com>
2024-10-25 08:52:02 +0200

    Create max.java

---
+++ max.java
@@ -0,0 +1,16 @@
+public class Main {
+  public int max (2,3) {
+
+    public static void main (String[]args) {
+
+      int max (int a, int b);
+
+      if (a > b) {
+        return a;
+      }else if (a < b) {
+        return b;
+      }else {
+        return a;
+      }
+    }
+  }
